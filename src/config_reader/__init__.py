d21ef073035b2999381c33c855d2f0eef9c2eb58
Andrea Gemma <andrea.gemma@gmail.com>
2026-09-03 00:36:45 +0200

    Add CI and release workflows, update versioning and exports

--- src/config_reader/__init__.py
+++ src/config_reader/__init__.py
@@ -3,4 +3,6 @@ Simple library functions for the project.
 """
 
 from ._version import __version__
-from .config_reader import *
+from .config_reader import ConfigReader, ConfigSource
+
+__all__ = ["__version__", "ConfigReader", "ConfigSource"]
